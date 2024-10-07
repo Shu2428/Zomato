426647caf808759927d273f44b0416ced273ec47
Shu2428 <shubhamrastogi1234king@gmail.com>
2024-10-07 15:03:01 +0530

    Created Zomato App

---
+++ Zomato.java
@@ -0,0 +1,10 @@
+abstract class Zomato
+{
+          public void displayAllProducts()
+          {
+                System.out.println("All Products Displayed");
+          }
+
+          public void addProductToCart(int id);
+          
+}
